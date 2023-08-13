0d0c6592db58ccdd387da7e556dd64ba3a5854eb
İsa Sayar <isa.sayar1725@gmail.com>
2023-08-13 23:42:50 +0300

    Validation was added.

--- ProductsService/src/main/java/com/syrisa/tr/productsservice/command/rest/CreateProductRestModel.java
+++ ProductsService/src/main/java/com/syrisa/tr/productsservice/command/rest/CreateProductRestModel.java
@@ -2,11 +2,18 @@ package com.syrisa.tr.productsservice.command.rest;
 
 import lombok.*;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
 import java.math.BigDecimal;
 
 @Data
 public class CreateProductRestModel {
+    @NotBlank(message = "Product title is a required field")
     private String title;
+    @Min(value = 1,message = "Product quantity cannot be less than 1")
+    @Max(value = 5,message = "Product quantity cannot be greater than 100")
     private Integer quantity;
+    @Min(value = 1,message = "Product price cannot be less than 1")
     private BigDecimal price;
 }
