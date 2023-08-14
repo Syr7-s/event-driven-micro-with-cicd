7252da148da71b91796b06e20d00d04cb1c91ea8
İsa Sayar <isa.sayar1725@gmail.com>
2023-08-14 22:24:26 +0300

    CreateProducCommandInterceptor named scrip was modified.

--- ProductsService/src/main/java/com/syrisa/tr/productsservice/command/interceptor/CreateProductCommandInterceptor.java
+++ ProductsService/src/main/java/com/syrisa/tr/productsservice/command/interceptor/CreateProductCommandInterceptor.java
@@ -1,6 +1,10 @@
 package com.syrisa.tr.productsservice.command.interceptor;
 
 import com.syrisa.tr.productsservice.command.CreateProductCommand;
+import com.syrisa.tr.productsservice.core.data.ProductEntity;
+import com.syrisa.tr.productsservice.core.data.ProductLookupEntity;
+import com.syrisa.tr.productsservice.core.data.ProductLookupRepository;
+import lombok.RequiredArgsConstructor;
 import org.axonframework.commandhandling.CommandMessage;
 import org.axonframework.messaging.MessageDispatchInterceptor;
 import org.slf4j.Logger;
@@ -13,38 +17,23 @@ import java.util.List;
 import java.util.function.BiFunction;
 
 @Component
+@RequiredArgsConstructor
 public class CreateProductCommandInterceptor implements MessageDispatchInterceptor<CommandMessage<?>> {
 
-    public static final Logger LOGGER = LoggerFactory.getLogger(CreateProductCommandInterceptor.class);
-   /* @Override
-    public CommandMessage<?> handle(CommandMessage<?> commandMessage) {
-        LOGGER.info("Intercepted command:  Function " + commandMessage.getPayloadType());
-        if (CreateProductCommand.class.equals(commandMessage.getPayloadType())) {
-            CreateProductCommand createProductCommand = (CreateProductCommand) commandMessage.getPayload();
-            if (createProductCommand.getPrice().compareTo(BigDecimal.ZERO)<=0){
-                throw new IllegalArgumentException("Price cannot be less than or equal to zero");
-            }
+    private final ProductLookupRepository productLookupRepository;
 
-            if (createProductCommand.getTitle()==null || createProductCommand.getTitle().isBlank()){
-                throw new IllegalArgumentException("Title cannot be empty");
-            }
-        }
-        return commandMessage;
-    }*/
+    public static final Logger LOGGER = LoggerFactory.getLogger(CreateProductCommandInterceptor.class);
 
     @Nonnull
     @Override
     public BiFunction<Integer, CommandMessage<?>, CommandMessage<?>> handle(@Nonnull List<? extends CommandMessage<?>> list) {
         return (index, command) -> {
-            LOGGER.info("Intercepted command:  NonNull Function " + command.getPayloadType());
+            LOGGER.info(String.format("Intercepted command %s",  command.getPayloadType()));
             if (CreateProductCommand.class.equals(command.getPayloadType())) {
                 CreateProductCommand createProductCommand = (CreateProductCommand) command.getPayload();
-                if (createProductCommand.getPrice().compareTo(BigDecimal.ZERO)<=0){
-                    throw new IllegalArgumentException("Price cannot be less than or equal to zero");
-                }
-
-                if (createProductCommand.getTitle()==null || createProductCommand.getTitle().isBlank()){
-                    throw new IllegalArgumentException("Title cannot be empty");
+                ProductLookupEntity  productLookupEntity= productLookupRepository.findByProductIdOrTitle(createProductCommand.getProductId(), createProductCommand.getTitle());
+                if (productLookupEntity != null) {
+                    throw new IllegalStateException(String.format("Product with productId %s or title %s already exists", createProductCommand.getProductId(), createProductCommand.getTitle()));
                 }
             }
             return command;
