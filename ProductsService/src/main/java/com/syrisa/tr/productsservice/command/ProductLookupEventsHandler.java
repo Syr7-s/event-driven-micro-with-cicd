57cf036ee769e57be35b6479805923cbbee825ad
İsa Sayar <isa.sayar1725@gmail.com>
2023-08-14 22:55:07 +0300

    ProductLookupEventsHandler script was modified.

--- ProductsService/src/main/java/com/syrisa/tr/productsservice/command/ProductLookupEventsHandler.java
+++ ProductsService/src/main/java/com/syrisa/tr/productsservice/command/ProductLookupEventsHandler.java
@@ -6,7 +6,6 @@ import com.syrisa.tr.productsservice.core.events.ProductCreatedEvent;
 import lombok.RequiredArgsConstructor;
 import org.axonframework.config.ProcessingGroup;
 import org.axonframework.eventhandling.EventHandler;
-import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Component;
 
 @Component
