29aa74299c201bcc06ff50f4335424eb7b97d619
İsa Sayar <isa.sayar1725@gmail.com>
2023-08-15 23:18:55 +0300

    Scripts were added to the OrderEventHandler

--- OrdersService/src/main/java/com/syrisa/tr/ordersservice/query/OrderEventsHandler.java
+++ OrdersService/src/main/java/com/syrisa/tr/ordersservice/query/OrderEventsHandler.java
@@ -1,7 +1,40 @@
 package com.syrisa.tr.ordersservice.query;
 
+import com.syrisa.tr.ordersservice.core.entity.OrderEntity;
+import com.syrisa.tr.ordersservice.core.entity.OrdersRepository;
+import com.syrisa.tr.ordersservice.core.events.OrderCreatedEvent;
+import lombok.RequiredArgsConstructor;
+import org.axonframework.config.ProcessingGroup;
+import org.axonframework.eventhandling.EventHandler;
+import org.axonframework.messaging.interceptors.ExceptionHandler;
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Component;
 
 @Component
+@ProcessingGroup("order-group")
+@RequiredArgsConstructor
 public class OrderEventsHandler {
+    private final OrdersRepository ordersRepository;
+
+    @ExceptionHandler(resultType = Exception.class)
+    public void handle(Exception exception) throws Exception {
+        throw exception;
+    }
+
+    @ExceptionHandler(resultType = IllegalArgumentException.class)
+    public void handle(IllegalArgumentException exception) {
+        System.out.println("Illegal Argument Exception: " + exception.getMessage());
+    }
+
+    @EventHandler
+    public void on(OrderCreatedEvent orderCreatedEvent) {
+        OrderEntity orderEntity = new OrderEntity();
+        BeanUtils.copyProperties(orderCreatedEvent, orderEntity);
+        try {
+            ordersRepository.save(orderEntity);
+        } catch (IllegalArgumentException ex) {
+            ex.printStackTrace();
+        }
+
+    }
 }
