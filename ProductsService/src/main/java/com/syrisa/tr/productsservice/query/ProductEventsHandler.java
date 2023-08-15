6093acd6587b51fc819a97d6a507f4343d7370b2
İsa Sayar <isa.sayar1725@gmail.com>
2023-08-15 09:02:30 +0300

    ExceptionHandler annotation was added to ProductEventsHandler method

--- ProductsService/src/main/java/com/syrisa/tr/productsservice/query/ProductEventsHandler.java
+++ ProductsService/src/main/java/com/syrisa/tr/productsservice/query/ProductEventsHandler.java
@@ -6,6 +6,7 @@ import com.syrisa.tr.productsservice.core.events.ProductCreatedEvent;
 import lombok.RequiredArgsConstructor;
 import org.axonframework.config.ProcessingGroup;
 import org.axonframework.eventhandling.EventHandler;
+import org.axonframework.messaging.interceptors.ExceptionHandler;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Component;
 
@@ -16,10 +17,24 @@ public class ProductEventsHandler {
 
     private final ProductsRepository productsRepository;
 
+    @ExceptionHandler(resultType = Exception.class)
+    public void handle(Exception exception) {
+        System.out.println("Exception: " + exception.getMessage());
+    }
+
+    @ExceptionHandler(resultType = IllegalArgumentException.class)
+    public void handle(IllegalArgumentException exception) {
+        System.out.println("Illegal Argument Exception: " + exception.getMessage());
+    }
+
     @EventHandler
     public void on(ProductCreatedEvent event) {
         ProductEntity productEntity = new ProductEntity();
         BeanUtils.copyProperties(event, productEntity);
-        productsRepository.save(productEntity);
+        try {
+            productsRepository.save(productEntity);
+        } catch (IllegalArgumentException ex) {
+            ex.printStackTrace();
+        }
     }
 }
