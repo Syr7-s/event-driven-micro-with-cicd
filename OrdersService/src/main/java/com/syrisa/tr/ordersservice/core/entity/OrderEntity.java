4b9f77c8e0bf00ea94d2ef4e94ec2e0a094dab13
İsa Sayar <isa.sayar1725@gmail.com>
2023-08-15 23:14:06 +0300

    Some variables were added to OrdersEntity

--- OrdersService/src/main/java/com/syrisa/tr/ordersservice/core/entity/OrderEntity.java
+++ OrdersService/src/main/java/com/syrisa/tr/ordersservice/core/entity/OrderEntity.java
@@ -1,4 +1,23 @@
 package com.syrisa.tr.ordersservice.core.entity;
 
+import com.syrisa.tr.ordersservice.core.utils.OrderStatus;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "orders")
+@Getter
+@Setter
 public class OrderEntity {
+    @Id
+    @Column(unique = true)
+    public String orderId;
+    private String productId;
+    private String userId;
+    private int quantity;
+    private String addressId;
+    @Enumerated(EnumType.STRING)
+    private OrderStatus orderStatus;
 }
