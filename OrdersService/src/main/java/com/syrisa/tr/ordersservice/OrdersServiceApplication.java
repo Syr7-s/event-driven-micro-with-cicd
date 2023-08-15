95517e827a9a8e0b79b5f939e45136d211b23d18
İsa Sayar <isa.sayar1725@gmail.com>
2023-08-15 22:40:23 +0300

    EnableDiscoveryClient named annotation was added.

--- OrdersService/src/main/java/com/syrisa/tr/ordersservice/OrdersServiceApplication.java
+++ OrdersService/src/main/java/com/syrisa/tr/ordersservice/OrdersServiceApplication.java
@@ -2,8 +2,10 @@ package com.syrisa.tr.ordersservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient // It is used to register the service to the Eureka server.
 public class OrdersServiceApplication {
 
     public static void main(String[] args) {
