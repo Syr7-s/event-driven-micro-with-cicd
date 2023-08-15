ff7d7fdf11f829abfe612edfdd8d3b2a67eb3dc4
İsa Sayar <isa.sayar1725@gmail.com>
2023-08-15 23:00:26 +0300

    OrdersCommandController named class was created

--- OrdersService/src/main/java/com/syrisa/tr/ordersservice/command/rest/OrdersCommandController.java
+++ OrdersService/src/main/java/com/syrisa/tr/ordersservice/command/rest/OrdersCommandController.java
@@ -1,16 +1,34 @@
 package com.syrisa.tr.ordersservice.command.rest;
 
+import com.syrisa.tr.ordersservice.command.CreateOrderCommand;
 import lombok.RequiredArgsConstructor;
+import org.axonframework.commandhandling.gateway.CommandGateway;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/v1/order")
 @RequiredArgsConstructor
 public class OrdersCommandController {
+
+    private final CommandGateway commandGateway;
+
     @PostMapping
-    public String createOrder(){
-        return "HTTP Post Handled";
+    public String createOrder(@Valid @RequestBody CreateOrderRestModel createOrderRestModel){
+        String returnValue = "";
+        CreateOrderCommand createOrderCommand = CreateOrderCommand.builder()
+                .orderId(UUID.randomUUID().toString())
+                .productId(createOrderRestModel.getProductId())
+                .quantity(createOrderRestModel.getQuantity())
+                .userId("27b95829-4f3f-4ddf-8983-151ba010e35b")
+                .addressId(createOrderRestModel.getAddressId())
+                .build();
+        returnValue = commandGateway.sendAndWait(createOrderCommand);
+        return returnValue;
     }
 }
