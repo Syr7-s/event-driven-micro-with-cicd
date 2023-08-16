2458d103b7a1ffe39897c22bcba3ae048b1e2b95
İsa Sayar <isa.sayar1725@gmail.com>
2023-08-16 22:17:34 +0300

    ReserveProductCommand was created

---
+++ core/src/main/java/com/syrisa/tr/core/commands/ReserveProductCommand.java
@@ -0,0 +1,17 @@
+package com.syrisa.tr.core.commands;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import org.axonframework.modelling.command.TargetAggregateIdentifier;
+
+@Getter
+@Setter
+@Builder
+public class ReserveProductCommand {
+    @TargetAggregateIdentifier
+    private final String productId;
+    private final int quantity;
+    private final String orderId;
+    private final String userId;
+}
