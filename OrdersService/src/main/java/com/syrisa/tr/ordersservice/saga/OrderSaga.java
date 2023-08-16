b0a7937135d22fd4159084fc0d42aedd251850f7
İsa Sayar <isa.sayar1725@gmail.com>
2023-08-16 21:33:44 +0300

    handle method was added

--- OrdersService/src/main/java/com/syrisa/tr/ordersservice/saga/OrderSaga.java
+++ OrdersService/src/main/java/com/syrisa/tr/ordersservice/saga/OrderSaga.java
@@ -1,7 +1,10 @@
 package com.syrisa.tr.ordersservice.saga;
 
+import com.syrisa.tr.ordersservice.core.events.OrderCreatedEvent;
 import lombok.RequiredArgsConstructor;
 import org.axonframework.commandhandling.gateway.CommandGateway;
+import org.axonframework.modelling.saga.SagaEventHandler;
+import org.axonframework.modelling.saga.StartSaga;
 import org.axonframework.spring.stereotype.Saga;
 
 @Saga
@@ -10,4 +13,9 @@ public class OrderSaga {
 
     private final transient CommandGateway commandGateway;
 
+    @StartSaga // This annotation is used to start the saga.
+    @SagaEventHandler(associationProperty = "orderId") // This annotation is used to define the event that will be handled by the saga.
+    public void handle(OrderCreatedEvent orderCreatedEvent) {
+
+    }
 }
