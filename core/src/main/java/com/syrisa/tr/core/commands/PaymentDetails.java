39e2ff913abab684bf9a91019043b8e816436aca
İsa Sayar <isa.sayar1725@gmail.com>
2023-08-17 08:55:17 +0300

    PaymentDetails named class was added to the Core project

---
+++ core/src/main/java/com/syrisa/tr/core/commands/PaymentDetails.java
@@ -0,0 +1,16 @@
+package com.syrisa.tr.core.commands;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+public class PaymentDetails {
+    private final String name;
+    private final String cardNumber;
+    private final int validUntilMonth;
+    private final int validUntilYear;
+    private final String cvv;
+}
