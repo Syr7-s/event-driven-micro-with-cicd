0b6ab8cbe220fae483508995e1def8a023f94ca2
İsa Sayar <isa.sayar1725@gmail.com>
2023-08-17 08:52:18 +0300

    User Service was created

---
+++ UserService/src/test/java/com/syrisa/tr/userservice/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.syrisa.tr.userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
