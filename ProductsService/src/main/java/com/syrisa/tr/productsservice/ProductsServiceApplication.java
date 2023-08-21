8eecc5b828cf13e2d3d158178efe6bfb179a2903
İsa Sayar <isa.sayar1725@gmail.com>
2023-08-21 08:38:35 +0300

    productSnapshotTriggerDefinition was added

--- ProductsService/src/main/java/com/syrisa/tr/productsservice/ProductsServiceApplication.java
+++ ProductsService/src/main/java/com/syrisa/tr/productsservice/ProductsServiceApplication.java
@@ -7,11 +7,15 @@ import com.thoughtworks.xstream.XStream;
 import org.axonframework.commandhandling.CommandBus;
 import org.axonframework.config.EventProcessingConfigurer;
 import org.axonframework.eventhandling.PropagatingErrorHandler;
+import org.axonframework.eventsourcing.EventCountSnapshotTriggerDefinition;
+import org.axonframework.eventsourcing.SnapshotTriggerDefinition;
+import org.axonframework.eventsourcing.Snapshotter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 //@EnableJpaRepositories
@@ -31,5 +35,10 @@ public class ProductsServiceApplication {
     public void configure(EventProcessingConfigurer configurer){
         configurer.registerListenerInvocationErrorHandler("product-group",conf -> new ProductsServiceEventsHandler());
         // configurer.registerListenerInvocationErrorHandler("product-group",conf -> PropagatingErrorHandler.instance());
+    }
+
+    @Bean(name="productSnapshotTriggerDefinition")
+    public SnapshotTriggerDefinition productSnapshotTriggerDefinition(Snapshotter snapshotter){
+        return new EventCountSnapshotTriggerDefinition(snapshotter,3);
     }
 }
